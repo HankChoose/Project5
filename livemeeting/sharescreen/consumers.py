03225903dfd8885697c6929af49b8ff8971da065
Hank Chen <106391093+HankChoose@users.noreply.github.com>
2025-11-12 17:07:01 -0700

    2

--- livemeeting/sharescreen/consumers.py
+++ livemeeting/sharescreen/consumers.py
@@ -1,42 +1,66 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-class SharescreenConsumer(AsyncWebsocketConsumer):
+rooms = {}  # {room_name: {'owner': channel_name, 'viewers': set()}}
+
+class ShareScreenConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = f'sharescreen_{self.room_name}'
+        self.room_group_name = f"sharescreen_{self.room_name}"
 
-        # 加入房间组
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        # 创建房间字典
+        if self.room_name not in rooms:
+            rooms[self.room_name] = {'owner': None, 'viewers': set()}
 
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
-    async def disconnect(self, close_code):
-        # 离开房间组
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        # 记录自己身份
+        if rooms[self.room_name]['owner'] is None:
+            self.is_owner = True
+            rooms[self.room_name]['owner'] = self.channel_name
+            await self.send(json.dumps({'type': 'role', 'role': 'owner'}))
+        else:
+            self.is_owner = False
+            rooms[self.room_name]['viewers'].add(self.channel_name)
+            await self.send(json.dumps({'type': 'role', 'role': 'viewer'}))
 
-    # 接收 WebSocket 消息
+    async def disconnect(self, close_code):
+        if self.is_owner:
+            rooms[self.room_name]['owner'] = None
+            # 通知观看者共享者离开
+            for viewer in rooms[self.room_name]['viewers']:
+                await self.channel_layer.send(viewer, {
+                    'type': 'owner_left'
+                })
+            rooms[self.room_name]['viewers'].clear()
+        else:
+            rooms[self.room_name]['viewers'].discard(self.channel_name)
+
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
     async def receive(self, text_data):
         data = json.loads(text_data)
-        # 广播给房间里的其他用户
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'signal_message',
-                'message': data,
-                'sender_channel': self.channel_name
-            }
-        )
+        msg_type = data.get('type')
 
-    # 广播消息给组
+        if self.is_owner:
+            # owner 发出的消息转发给所有 viewers
+            for viewer in rooms[self.room_name]['viewers']:
+                await self.channel_layer.send(viewer, {
+                    'type': 'signal_message',
+                    'message': data
+                })
+        else:
+            # viewer 发给 owner
+            owner = rooms[self.room_name]['owner']
+            if owner:
+                await self.channel_layer.send(owner, {
+                    'type': 'signal_message',
+                    'message': data
+                })
+
     async def signal_message(self, event):
-        # 不发给自己
-        if event['sender_channel'] == self.channel_name:
-            return
         await self.send(text_data=json.dumps(event['message']))
+
+    async def owner_left(self, event):
+        await self.send(text_data=json.dumps({'type': 'owner_left'}))
